838ab6163cd6ec87495356ebcd6f95cce68b713b
americojm <americomotoz2018@gmail.com>
2023-06-30 03:29:17 -0400

    perzonalizandoTem

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[App\Http\Controllers\PortafolioController::class, 'index']);
+
+Auth::routes();
+
+Route::resource('/proyectos',App\Http\Controllers\ProyectoController::class);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
